ac5ab3c7bb354d5c14e45b5be16bcdef0b4749ed
wangxch320 <qhsd320@163.com>
2017-04-10 20:02:40 +0800

    first commit

---
+++ fileUploadByMVC/Controllers/TestController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using fileUploadByMVC.Models;
+using System.IO;
+
+namespace fileUploadByMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        private string _directory = @"/File";
+        Random rnd = new Random();
+        [HttpPost]
+        public ActionResult Upload(TestModels tm, HttpPostedFileBase file)
+        {
+            if(file == null)
+            {
+                return Content("没有上传任何文件");
+            }
+
+            string fileExt = Path.GetExtension(Request.Files[0].FileName).ToLower();
+            string fileName = Server.MapPath(_directory) + "\\" + DateTime.Now.ToString("yyyyMMddHHmmssfff") + rnd.Next(10, 99).ToString() + fileExt;
+            try
+            {
+                file.SaveAs(fileName);
+                tm.AttachmentPath = "../upload/" + Path.GetFileName(file.FileName);
+                return RedirectToAction("Show", tm);
+            }
+            catch
+            {
+                return Content("上传异常 ！", "text/plain");
+            }
+        }
+
+        public ActionResult Show(TestModels tm)
+        {
+            return View(tm);
+        }
+
+    }
+}
---
+++ fileUploadByMVC/Models/TestModels.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace fileUploadByMVC.Models
+{
+    public class TestModels
+    {
+        [Display(Name ="标题")]
+        [Required]
+        public string title
+        {
+            get;
+            set;
+        }
+
+        [Display(Name ="内容")]
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Content
+        {
+            get;
+            set;
+        }
+
+        public string AttachmentPath
+        {
+            get;
+            set;
+        }
+    }
+}
